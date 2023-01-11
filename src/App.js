d96857cedf66ae0c404ca1913c29e027ee93be0c
Ilya <ilya.kiryk@gmail.com>
2023-01-11 08:56:36 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, {useState} from 'react'
+import logo from './logo.svg';
 import './App.css';
-import MainField from './Components/MainField';
-import Selection from './Components/Selection';
-import Router from './Router';
-
 
 function App() {
   return (
     <div className="App">
-      <Router />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
